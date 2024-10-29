7b833e079238da80d1f99542cd9154060335be2e
galinamiteva <galina.miteva@utb.ecutbildning.se>
2024-10-29 12:15:01 +0100

    redo

--- EmailProvider_Rika/Program.cs
+++ EmailProvider_Rika/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Communication.Email;
+using EmailProvider_Rika.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,10 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddSingleton<EmailClient>(new EmailClient(Environment.GetEnvironmentVariable("CommunicationServices")));
+        services.AddSingleton<IEmailService, EmailService>();
+
+
     })
     .Build();
 
---
+++ EmailProvider_Rika/Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿
+using Azure.Messaging.ServiceBus;
+using EmailProvider_Rika.Models;
+
+namespace EmailProvider_Rika.Services;
+
+public interface IEmailService
+{
+    EmailRequest UnpackEmailRequest(ServiceBusReceivedMessage message);
+
+    bool SendEmail(EmailRequest emailRequest);
+}
